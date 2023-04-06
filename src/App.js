cfd4af4ab31a0c0fc82ed0da35e65f45c9fe984e
Arthur Teixeira <arthursony1@hotmail.com>
2023-04-06 17:03:07 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -12,6 +12,27 @@ class App extends React.Component {
     image: '',
     rare: '',
     trunfo: false,
+    disableBtn: true,
+  };
+
+  disableBtn = () => {
+    const maxValueAttr = 90;
+    const maxValueSum = 210;
+    const { name, description, attr1, attr2, attr3, image } = this.state;
+
+    const valText = name.length > 0
+    && description.length > 0
+    && image.length > 0;
+
+    const valNumber = Number(attr1) >= 0 && Number(attr1) <= maxValueAttr
+    && Number(attr2) >= 0 && Number(attr2) <= maxValueAttr
+    && Number(attr3) >= 0 && Number(attr3) <= maxValueAttr
+    && Number(attr1) + Number(attr2) + Number(attr3) <= maxValueSum;
+
+    console.log(Number(attr1) >= 0);
+    this.setState({
+      disableBtn: !(valNumber && valText),
+    });
   };
 
   HandleChange = ({ target }) => {
@@ -19,11 +40,12 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.disableBtn);
   };
 
   render() {
-    const { name, description, attr1, attr2, attr3, image, rare, trunfo } = this.state;
+    const { name, description, attr1, attr2, attr3,
+      image, rare, trunfo, disableBtn } = this.state;
     return (
       <div>
         <Form
@@ -36,6 +58,7 @@ class App extends React.Component {
           cardRare={ rare }
           cardTrunfo={ trunfo }
           onInputChange={ this.HandleChange }
+          isSaveButtonDisabled={ disableBtn }
         />
         <Card
           cardName={ name }
